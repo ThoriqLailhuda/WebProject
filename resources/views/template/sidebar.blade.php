8c3d14aab06e5d09decbacafecc0a41330cee15c
sahdewibungasafira <sahdewibunga@student.uns.ac.id>
2021-05-30 14:27:22 +0700

    sidebar edit

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -24,8 +24,8 @@
                     <a class="list-group-item list-group-item-action bg-light" href="{{url('/daftarpasien')}}">Lihat Data</a>
                     <a class="list-group-item list-group-item-action bg-light" href="{{url('/reservasi_pasien')}}">reservasi</a>
                     <a class="list-group-item list-group-item-action bg-light" href="{{url('/kunjungan')}}">Kunjungan</a>
-                    <a class="list-group-item list-group-item-action bg-light" href="{{url('/dokter')}}">dokter</a>
-                    <a class="list-group-item list-group-item-action bg-light" href="{{url('/perawat')}}">perawat</a>
+                    <a class="list-group-item list-group-item-action bg-light" href="{{url('/dokter')}}">Dokter</a>
+                    <a class="list-group-item list-group-item-action bg-light" href="{{url('/perawat')}}">Perawat</a>
                     <a class="list-group-item list-group-item-action bg-light" href="{{url('/kunjungan_poli')}}">Kunjungan Poli</a>
                 </div>
                 
