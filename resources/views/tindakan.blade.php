8bfbfcda5786a3bb6561ba84fa8c9c66b07d9285
sahdewibungasafira <sahdewibunga@student.uns.ac.id>
2021-05-31 16:06:52 +0700

    revisi

---
+++ resources/views/tindakan.blade.php
@@ -0,0 +1,33 @@
+@extends('template.sidebar')
+
+@section('content')
+
+<div class="container">
+
+<form action="{{url('daftar_kunjungan')}}" method="post" required>
+                                {{csrf_field()}}
+                                Id
+                                <input type="text" class="form-control" name="id" required>
+                                Id Periksa Poli
+                                <input type="text" class="form-control" name="id_periksa_poli" required>
+                                Id Tindakan
+                                <input type="text" class="form-control" name="id_tindakan" required>
+                                Harga
+                                <input type="int" class="form-control" name="harga" required>
+                                Jumlah
+                                <input type="int" class="form-control" name="jml" required>
+                                Create by 
+                                <input type="text" class="form-control" name="cretae_by" required>
+                                Create add
+                                <input type="text" class="form-control" name="create_add" required>
+                                Edited by
+                                <input type="text" class="form-control" name="edit_by" required>
+                                Edited Add
+                                <input type="text" class="form-control" name="edit_add" required>
+
+                                <br>
+                                <input type="SUBMIT" class="btn btn-primary">   
+
+                                </form>
+</div>
+@endsection
